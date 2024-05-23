322338418f40d9134bb2dcef9338b62a3f82894c
Bogdan Kravchenko <bkblogpost@gmail.com>
2024-05-23 12:29:18 +0300

    sec

--- conftest.py
+++ conftest.py
@@ -1,6 +1,13 @@
-def main():
-    pass
-    
-    
-if __name__ == '__main__':
-    main()
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+@pytest.fixture(scope="function")
+def driver():
+    service = Service(executable_path=ChromeDriverManager.install())
+    driver = webdriver.Chrome(service=service)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
